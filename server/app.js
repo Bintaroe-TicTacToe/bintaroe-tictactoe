947f1d855c9f9863853a63d6632d8bf6d2f32176
rafiframa <rafiframa@gmail.com>
2024-07-17 13:04:33 +0700

    ainGame

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const io = new Server({
   }
 });
 
-function checkWinner(board) {
+function checkResult(board) {
   const winningCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertikal
@@ -32,37 +32,48 @@ function checkWinner(board) {
 let game = null
 
 io.on("connection", (socket) => {
-  if (game !== null) {
+  // console.log(game)
+  if (game) {
     game.players.push(socket.id)
+    io.emit('gameState', game)
+  } else{
+    game = {
+      players: [socket.id],
+      board: [null, null, null, null, null, null, null, null, null],
+      currentPlayer: 0,
+      gameOver: false,
+      result: null
+    };
+    // console.log(game.board)
+    io.emit('gameState', game)
+
   }
-  game = {
-    players: [socket.id],
-    board: [null, null, null, null, null, null, null, null, null],
-    currentPlayer: 0,
-    gameOver: false,
-    winner: null
-  };
-  // console.log(game.board)
-  socket.emit('gameState', game)
+  
 
 
 
   socket.on("makeMove", (index) => {
-    game.board[index] = (game.currentPlayer === 0) ? "X" : "O"
-    const winner = checkWinner(game.board)
-    if (winner) {
-      game.gameOver = true,
-        game.winner = winner
-        console.log("winner is", (game.currentPlayer === 0) ? "X" : "O")
-    }
-    else {
-      console.log("from", game.currentPlayer)
-      game.currentPlayer = (game.currentPlayer === 0) ? 1 : 0;
-      console.log("to", game.currentPlayer)
-    }
+    if (game.gameOver === false) {
+      game.board[index] = (game.currentPlayer === 0) ? "X" : "O"
+      const result = checkResult(game.board)
+      if (result) {
+        game.gameOver = true,
+          game.result = result
+        console.log(result)
+      }
+      else {
+        game.currentPlayer = (game.currentPlayer === 0) ? 1 : 0;
+      }
 
-    socket.emit('gameState', game)
+      io.emit('gameState', game)
+
+    }
   })
+
+  socket.on('disconnect', () => {
+
+  });
+
 });
 
 io.listen(3000);
